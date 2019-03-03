810896ea2b224d192879cf3848cf1053d49835d0
gabdullah <gabdull2@kent.edu>
2019-03-03 05:04:52 -0500

    lol

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
   <body>
+    <div>{{err}}</div>
     <div id="grid"></div>
   </body>
 </template>
@@ -13,6 +14,9 @@ export default {
       grid: '',
       row: '',
       column: '',
+      err: '',
+      ox: -1,
+      oy: -1,
     }
   },
   props: {
@@ -26,19 +30,40 @@ export default {
       var width = 50;
       var height = 50;
       var click = 0;
-  
+      var playerRand = Math.floor(Math.random() * 10);
+      var exitRand = Math.floor(Math.random() * 10);
+      var player = false;
+      var exit = false;
+
       // iterate for rows 
       for (var row = 0; row < 10; row++) {
         data.push( new Array() );
     
         // iterate for cells/columns inside rows
         for (var column = 0; column < 10; column++) {
+          var rand = Math.floor(Math.random() * 10);
+          var obstacle = false;
+          if (rand < 3) obstacle = true;
+          if (column == 0 && row == playerRand) {
+            console.log(column, row)
+            player = true;
+          }
+          else player = false;
+          if (column == 9 && row == exitRand) {
+            console.log(column, row)
+            exit = true;
+          }
+          else exit = false;
+
           data[row].push({
             x: xpos,
             y: ypos,
             width: width,
             height: height,
-            click: click
+            click: click,
+            obstacle: obstacle,
+            player: player,
+            exit: exit,
           })
           // increment the x position. I.e. move it over by 50 (width variable)
           xpos += width;
@@ -49,6 +74,129 @@ export default {
         ypos += height; 
       }
       return data;
+    },
+
+    check(direction) { 
+      var result = true;
+
+      this.column.each((d) => {
+        if (direction == 'left') {
+          if (this.ox == 1) result = false;
+          if ((this.ox - 50 == d.x) && d.obstacle && this.oy == d.y) {
+            result = false;
+          }
+        }
+        else if (direction == 'right') {
+          if (this.ox == 451) result = false;
+          if ((this.ox + 50 == d.x) && d.obstacle && this.oy == d.y) {
+            result = false;
+          }
+        }
+        else if (direction == 'up') {
+          if (this.oy == 1) result = false;
+          if ((this.oy - 50 == d.y) && d.obstacle && this.ox == d.x) {
+            result = false;
+          }
+        }
+        else if (direction == 'down') {
+          if (this.oy == 451) result = false;
+          if ((this.oy + 50 == d.y) && d.obstacle && this.ox == d.x) {
+            result = false;
+          }
+        }
+      });
+      return result;
+    },
+
+    moveRight() {
+      this.getPlayerLoc();
+      if (this.check('right')) {
+        this.column
+          .each((d) => {
+            if (this.ox == d.x && this.oy == d.y)
+              d.player = false;
+
+            else if ((this.ox + 50) == d.x && this.oy == d.y)
+              d.player = true;
+          });
+          this.update();
+      }
+      else {
+        this.err = 'Cannot move right';
+      }
+    },
+
+    moveLeft() {
+      this.getPlayerLoc();
+      if (this.check('left')) {
+        this.column
+          .each((d) => {
+            if (this.ox == d.x && this.oy == d.y)
+              d.player = false;
+
+            else if ((this.ox - 50) == d.x && this.oy == d.y)
+              d.player = true;
+          });
+          this.update();
+      }
+      else {
+        this.err = 'Cannot move left';
+      }
+    },
+
+    moveUp() {
+      this.getPlayerLoc();
+      if (this.check('up')) {
+        this.column
+          .each((d) => {
+            if (this.ox == d.x && this.oy == d.y)
+              d.player = false;
+
+            else if (this.ox == d.x && (this.oy - 50) == d.y)
+              d.player = true;
+          });
+          this.update();
+      }
+      else {
+        this.err = 'Cannot move up';
+      }
+    },
+
+    moveDown() {
+      this.getPlayerLoc();
+      if (this.check('down')) {
+        this.column
+          .each((d) => {
+            if (this.ox == d.x && this.oy == d.y)
+              d.player = false;
+
+            else if (this.ox == d.x && (this.oy + 50) == d.y)
+              d.player = true;
+          });
+          this.update();
+      }
+      else {
+        this.err = 'Cannot move down';
+      }
+    },
+
+    getPlayerLoc() {
+      this.column.each((d) => {
+        if (d.player) {
+          this.ox = d.x;
+          this.oy = d.y;
+        }
+      });
+    },
+
+    update() {
+      this.column
+        .attr("xlink:href", (d) => {
+          if (d.player) { return "https://i.imgur.com/2BgE0Gt.png" }
+          else if (d.exit) { return "https://i.imgur.com/X0SIYn3.png" }
+          else if (d.obstacle) { return "https://i.imgur.com/9z6Yclr.png" }
+          else { return "https://i.imgur.com/y4lTohA.png" }
+        });
     }
   },
   mounted() {
@@ -60,8 +208,8 @@ export default {
 
     this.grid = d3.select("#grid")
       .append("svg")
-      .attr("width","510px")
-      .attr("height","510px");
+      .attr("width","500px")
+      .attr("height","500px");
 
     this.row = this.grid.selectAll(".row")
       .data(this.gridData)
@@ -70,22 +218,27 @@ export default {
 
     this.column = this.row.selectAll(".square")
       .data(function(d) { return d; })
-      .enter().append("rect")
-      .attr("class","square")
+      //.enter().append("rect")
+      .enter().append("svg:image")
+      //.attr("class","square")
+      .attr("xlink:href", (d) => {
+        if (d.player) { return "https://i.imgur.com/2BgE0Gt.png" }
+        else if (d.exit) { return "https://i.imgur.com/X0SIYn3.png" }
+        else if (d.obstacle) { return "https://i.imgur.com/9z6Yclr.png" }
+        else { return "https://i.imgur.com/y4lTohA.png" }
+      })
       .attr("x", function(d) { return d.x; })
       .attr("y", function(d) { return d.y; })
       .attr("width", function(d) { return d.width; })
-      .attr("height", function(d) { return d.height; })
-      .style("fill", "#fff")
-      .style("stroke", "#222")
-      .on('click', function(d) {
-        d.click ++;
-        if ((d.click)%4 == 0 ) { d3.select(this).style("fill","#fff"); }
-        if ((d.click)%4 == 1 ) { d3.select(this).style("fill","#2C93E8"); }
-        if ((d.click)%4 == 2 ) { d3.select(this).style("fill","#F56C4E"); }
-        if ((d.click)%4 == 3 ) { d3.select(this).style("fill","#838690"); }
-        });
-    }, 1000)
+      .attr("height", function(d) { return d.height; 
+      });
+      
+    }, 300)
+    
+    setTimeout(() => { this.moveRight() }, 1500)
+    setTimeout(() => { this.moveDown() }, 3000)
+    setTimeout(() => { this.moveLeft() }, 4500)
+    setTimeout(() => { this.moveUp() }, 6000)
   },
 };
 </script>
